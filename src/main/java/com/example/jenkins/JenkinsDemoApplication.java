17ca8badbe886ac816f140f51d0dd24cd9179bfc
harshil-p-7span <harshil.p@7span.com>
2023-11-22 17:33:33 +0530

    Add lombok.

--- src/main/java/com/example/jenkins/JenkinsDemoApplication.java
+++ src/main/java/com/example/jenkins/JenkinsDemoApplication.java
@@ -1,18 +1,19 @@
 package com.example.jenkins;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class JenkinsDemoApplication {
 
-	public static final Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
 	public static void main(String[] args) {
-		logger.info("Application is running");
+		log.info("Application is running");
 		SpringApplication.run(JenkinsDemoApplication.class, args);
-		logger.info("Second logger");
+		log.info("Second logger");
 	}
 
 }
