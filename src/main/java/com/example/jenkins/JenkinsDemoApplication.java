44c3c875b17f4416c6391d2b5968562e13426974
harshil-p-7span <harshil.p@7span.com>
2023-11-22 16:30:03 +0530

    Add logger.

--- src/main/java/com/example/jenkins/JenkinsDemoApplication.java
+++ src/main/java/com/example/jenkins/JenkinsDemoApplication.java
@@ -1,14 +1,17 @@
 package com.example.jenkins;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsDemoApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
-		System.out.println("Running");
+		logger.info("Application is running");
 	}
 
 }
